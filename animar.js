22b593ebb4a1b0ea19a45a16a9aea616496bc77b
Mário Moisés <moisesmario61@gmail.com>
2022-09-29 04:02:32 +0200

    Atualização

--- animar.js
+++ animar.js
@@ -1,6 +1,8 @@
 
 const menuDiv = document.getElementById('menu-mobile')
 const btnanimar = document.getElementById('btn-menu')
+const nav=document.getElementsByTagName("nav")[0];
+const topoNav=nav.offsetTop;
 
 menuDiv.addEventListener('click', animar)
 
@@ -11,3 +13,18 @@ function animar(){
     const btn = document.getElementById('btn-menu')
     btn.classList.toggle('ativar')
 }
+
+/*
+window.onscroll=function(){
+    fixarMenuNoTopo();
+}
+
+function fixarMenuNoTopo(){
+    if(window.pageYOffset >= topoNav){
+         nav.classList.add("fixoNoTopo")
+    }
+    else{
+        nav.classList.remove("fixoNoTopo")
+    }
+}
+*/
